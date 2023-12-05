e1827d85a8bf16ddb383754ea64c49f9749cfda4
Рустам Ивазов <rustam@MacBook-Air-Rustam.local>
2023-12-05 23:06:00 +0300

    Ready for third review

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+
+urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
